f6073bb7512782a166611773ae269bf1a89edde0
Jesus Castañeda <>
2017-10-10 21:49:42 -0500

    Adding lodash with npm

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 const user = os.userInfo();
 console.log('Result:', notes.add(5, 4));
+
+console.log(_.isString('Jesus'));
+console.log(_.isString(user));
+console.log(_.isString(30));
+
+const filteredArray = _.uniq(['Jesus', 1, 'Jesus', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}, you are ${notes.age}!`);
